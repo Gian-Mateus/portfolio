a660f9a3af07065d6a3b56a35b402a42b6ac52ed
Gian Mateus <gian.m.silver@gmail.com>
2024-04-04 13:15:32 -0300

    teste 1

--- api/public/index.php
+++ api/public/index.php
@@ -7,7 +7,7 @@ require_once __DIR__.'/../../vendor/autoload.php';
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="shortcut icon" href="./assets/svgs/escanor-logo.svg" type="image/x-icon">
-    <link rel="stylesheet" href="/assets/style.css">
+    <link rel="stylesheet" href="./assets/style.css">
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
     <title>Gian Mateus- Desenvolvedor Web</title>
   </head>
